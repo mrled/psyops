d0df61594cc744174d10da0bd3a23c2cdcccf270
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-05-30 14:31:20 -0500

    run.ps1: Do not require volume path

--- run.ps1
+++ run.ps1
@@ -1,15 +1,18 @@
 # Docker can be a little precious sometimes
 # https://lmonkiewicz.com/programming/get-noticed-2017/docker-problems-on-windows/
 Param(
-    [Parameter(Mandatory=$true)] $psycheVolume,
-    [Parameter(Mandatory=$true)] $dockerImage
+    [Parameter(Mandatory=$true)] $dockerImage,
+    $psycheVolume
 )
 
-$psycheVolume = Resolve-Path $psycheVolume | Select-Object -ExpandProperty Path
+if ($psycheVolume) {
+    $psycheVolume = Resolve-Path $psycheVolume | Select-Object -ExpandProperty Path
+    $psycheVolumeArg = "-v ${psycheVolume}:/psyche"
+}
 $origMsysNoPathconv = $env:MSYS_NO_PATHCONV
 $env:MSYS_NO_PATHCONV = 1
 try {
-    docker run -v ${psycheVolume}:/psyche -it $dockerImage
+    docker run --rm $psycheVolumeArg -it $dockerImage
 } finally {
     $env:MSYS_NO_PATHCONV = $origMsysNoPathconv
 }
