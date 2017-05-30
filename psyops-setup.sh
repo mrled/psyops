92b088cfea67f6b48fa0264935b186655c5173f9
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-05-30 14:34:18 -0500

    psyops-setup.sh: Set key type as argument, with default

--- psyops-setup.sh
+++ psyops-setup.sh
@@ -6,39 +6,39 @@ set -u
 psyops=$(printf "$(ansi fg=magenta)P$(ansi fg=red)S$(ansi fg=yellow)Y$(ansi fg=green)O$(ansi fg=cyan)P$(ansi fg=blue)S$(ansi mode=reset)")
 
 usage() {
-    cat << ENDUSAGE
+    cat <<ENDUSAGE
 Usage: $0 [-h|--help] [-f|--force]
 Set up $psyops
     -h | --help: Print help and exit
     -f | --force: Remove any existing config files and reinit
+    -t | --ssh-key-type: Type of SSH key to expect/generate
 
 Configure the $psyops environment (gandi, digitalocean, etc)
 ENDUSAGE
 }
 
 force=
-if test $# -eq 1; then
+keytype=ed25519
+while test $# -gt 0; do
     case "$1" in
         -h | --help ) usage; exit;;
-        -f | --force ) force=1;;
-        *) usage; exit 1;;
+        -f | --force ) force=1; shift;;
+        -t | --ssh-key-type ) keytype=$2; shift 2;;
+        *) echo "Unknown argument: '$1'"; usage; exit 1;;
     esac
-elif test $# -gt 0; then
-    usage
-    exit 1
-fi
+done
 
 gandicfg=$HOME/.config/gandi/config.yaml
 doctlcfg=$HOME/.config/doctl/config.yaml
-idrsa=$HOME/.ssh/id_rsa
+keypath=$HOME/.ssh/$keytype
 
 if test -e "$gandicfg" && test "$force"; then rm "$gandicfg"; fi
 if test -e "$doctlcfg" && test "$force"; then rm "$doctlcfg"; fi
 # Don't let -f delete an existing SSH key
 
-if test ! -e "$idrsa"; then
+if test ! -e "$keypath"; then
     echo "Create SSH key for $psyops"
-    ssh-keygen -f "$idrsa" -t ed25519
+    ssh-keygen -f "$keypath" -t "$keytype"
 else
     echo "SSH key for $psyops already exists"
 fi
