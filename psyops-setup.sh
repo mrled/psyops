3297147d81065902378fa819705dd03e0a52cadc
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-06-02 11:23:37 -0500

    Set SSH key comment in psyops-setup

--- psyops-setup.sh
+++ psyops-setup.sh
@@ -12,6 +12,7 @@ Set up $psyops
     -h | --help: Print help and exit
     -f | --force: Remove any existing config files and reinit
     -t | --ssh-key-type: Type of SSH key to expect/generate
+    -C | --ssh-key-comment: Comment field of SSH key
 
 Configure the $psyops environment (gandi, digitalocean, etc)
 ENDUSAGE
@@ -19,11 +20,13 @@ ENDUSAGE
 
 force=
 keytype=ed25519
+keycomm=conspirator@PSYOPS
 while test $# -gt 0; do
     case "$1" in
         -h | --help ) usage; exit;;
         -f | --force ) force=1; shift;;
         -t | --ssh-key-type ) keytype=$2; shift 2;;
+        -C | --ssh-key-comment ) keycomm=$2; shift 2;;
         *) echo "Unknown argument: '$1'"; usage; exit 1;;
     esac
 done
@@ -38,7 +41,7 @@ if test -e "$doctlcfg" && test "$force"; then rm "$doctlcfg"; fi
 
 if test ! -e "$keypath"; then
     echo "Create SSH key for $psyops"
-    ssh-keygen -f "$keypath" -t "$keytype"
+    ssh-keygen -f "$keypath" -t "$keytype" -C "$keycomm"
 else
     echo "SSH key for $psyops already exists"
 fi
