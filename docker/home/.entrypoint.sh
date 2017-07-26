0aa5ad09517f442d1a77ac40cb1efba7c4d1d2e5
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-07-26 15:01:38 -0500

    Improve psecrets integration, login messages
    - Rename unlock-psecrets to psecretsctl and add functionality
    - Use psecretsctl to test for unlocked secrets
    - Improve message printed at container start

--- docker/home/.entrypoint.sh
+++ docker/home/.entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# If unlock-psecrets exits with an error, including from ctrl-c, dot-sourcing
+# If psecretsctl exits with an error, including from ctrl-c, dot-sourcing
 # here it prevents it from killing the whole shell
 # Then running it in a subshell prevents its variables from polluting my shell
-(. /usr/local/bin/unlock-psecrets)
+(. /usr/local/bin/psecretsctl unlock)
 
 exec /bin/bash -i
