c02516de9b5d2d423e50c7bb51a095198caaca0d
Micah R Ledbetter <me@micahrl.com>
2017-10-18 00:44:53 -0500

    Set $HOME properly

--- architect/architect-jenkins/entrypoint.sh
+++ architect/architect-jenkins/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
 usermod --append --groups "${DOCKER_HOST_DOCKER_GID}" "${JENKINS_USER}"
+
+# Since we pass --preserve-env to sudo below,
+# try to at least set $HOME correctly
+HOME=$(grep "^${JENKINS_USER}:x:" /etc/passwd | cut -d: -f6)
+if test -z "$HOME"; then
+    echo "Could not determine home directory for '${JENKINS_USER}'"
+    exit 1
+else
+    echo "Set \$HOME to ${HOME}"
+fi
+
 exec sudo --preserve-env --user="${JENKINS_USER}" /usr/local/bin/jenkins.sh "$@"
