7230619928ac54d391169dd43c660fb7f28c5e1a
Micah R Ledbetter <me@micahrl.com>
2020-05-18 11:16:25 -0500

    Don't log temp to console for cron - that sends an email

--- ansible/roles/base/files/rpi_log_temperature.py
+++ ansible/roles/base/files/rpi_log_temperature.py
@@ -8,42 +8,42 @@ import sys
 import gpiozero
 
 
-def initlogging():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    conshandl = logging.StreamHandler()
-    conshandl.setFormatter(
-        logging.Formatter('[Console] %(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    conshandl.setLevel(logging.INFO)
-    logger.addHandler(conshandl)
-    return logger
-
-
-LOGGER = initlogging()
-
-
 def main(args):
     parser = argparse.ArgumentParser(
         "Log the temperature of the Raspberry Pi to a remote syslog server")
     parser.add_argument("--syslog-server")
     parser.add_argument("--syslog-port", default=514, type=int)
     parser.add_argument("--local-syslog", action="store_true")
+    parser.add_argument("--no-console-log", action="store_true")
     parsed = parser.parse_args(args[1:])
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    if not parsed.no_console_log:
+        conshandl = logging.StreamHandler()
+        conshandl.setFormatter(
+            logging.Formatter('[Console] %(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        conshandl.setLevel(logging.INFO)
+        logger.addHandler(conshandl)
+
     if parsed.syslog_server:
         udpsyslhandl = logging.handlers.SysLogHandler(
             address=(parsed.syslog_server, parsed.syslog_port))
         udpsyslhandl.setFormatter(
             logging.Formatter('%(message)s'))
         udpsyslhandl.setLevel(logging.INFO)
-        LOGGER.addHandler(udpsyslhandl)
+        logger.addHandler(udpsyslhandl)
+
     if parsed.local_syslog:
         locsyslhandl = logging.handlers.SysLogHandler(address='/dev/log')
         locsyslhandl.setFormatter(
             logging.Formatter('%(message)s'))
         locsyslhandl.setLevel(logging.INFO)
-        LOGGER.addHandler(locsyslhandl)
+        logger.addHandler(locsyslhandl)
+
     cputemp = gpiozero.CPUTemperature().temperature
-    LOGGER.info(f"Raspberry Pi CPU temperature: {cputemp}C")
+    logger.info(f"Raspberry Pi CPU temperature: {cputemp}C")
 
 
 if __name__ == '__main__':
