8e02286c2bf635f4029e6d3ff45d32e4232dc00e
Micah R Ledbetter <me@micahrl.com>
2022-06-22 19:36:19 -0500

    Improve psyopsOS-init.sh
    Add better error handling, better logging, and --force-reinstall
    
    Don't pull down the vestigial node config json

--- psyopsOS/os-overlay/usr/local/sbin/psyopsOS-init.sh
+++ psyopsOS/os-overlay/usr/local/sbin/psyopsOS-init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
 
+dying_breath() {
+    logger --stderr --priority user.error --tag psyopsOS-init.sh "Script exiting with error"
+}
+
+trap dying_breath ERR
+
 usage() {
     cat <<ENDUSAGE
 usage: $0 [-h]
@@ -21,33 +27,36 @@ fi
 . /etc/psyopsOS/psyops-secret.env
 . /etc/psyopsOS/init.env
 
-logger --stderr --priority user.debug --tag psyopsOS-postboot "Fetching node configuration from $PSYOPSOS_NODE_CONFIG"
-node_json=/etc/psyopsOS/node.json
-curl -o "$node_json" "$PSYOPSOS_NODE_CONFIG"
-curl -o "$node_json.minisig" "$PSYOPSOS_NODE_CONFIG.minisig"
-if minisign -V -p "$PSYOPSOS_MINISIGN_PUB" -m "$node_json"; then
-    logger --stderr --priority user.debug --tag psyopsOS-postboot "node.json passed signature validation"
-else
-    logger --stderr --priority user.error --tag psyopsOS-postboot "node.json FAILED signature validation"
-fi
+# logger --stderr --priority user.debug --tag psyopsOS-init.sh "Fetching node configuration from $PSYOPSOS_NODE_CONFIG"
+# node_json=/etc/psyopsOS/node.json
+# curl -o "$node_json" "$PSYOPSOS_NODE_CONFIG"
+# curl -o "$node_json.minisig" "$PSYOPSOS_NODE_CONFIG.minisig"
+# if minisign -V -p "$PSYOPSOS_MINISIGN_PUB" -m "$node_json"; then
+#     logger --stderr --priority user.debug --tag psyopsOS-init.sh "node.json passed signature validation"
+# else
+#     logger --stderr --priority user.error --tag psyopsOS-init.sh "node.json FAILED signature validation"
+# fi
 
 progfig=progfiguration-0.0.0-py3-none-any.whl
 progfig_path=/var/psyopsOS/"$progfig"
 curl -o "$progfig_path" "$PSYOPSOS_SITE/psyopsOS/$progfig"
 curl -o "$progfig_path.minisig" "$PSYOPSOS_SITE/psyopsOS/$progfig.minisig"
 if minisign -V -p "$PSYOPSOS_MINISIGN_PUB" -m "$progfig_path"; then
-    logger --stderr --priority user.debug --tag psyopsOS-postboot "progfiguration package passed signature validation"
+    logger --stderr --priority user.debug --tag psyopsOS-init.sh "progfiguration package passed signature validation"
 else
-    logger --stderr --priority user.error --tag psyopsOS-postboot "progfiguration package FAILED signature validation"
+    logger --stderr --priority user.error --tag psyopsOS-init.sh "progfiguration package FAILED signature validation"
+    mkdir /var/psyopsOS/FAILED_SIG_VALIDATION
+    mv "$progfig_path" /var/psyopsOS/FAILED_SIG_VALIDATION
+    exit 1
 fi
 
-logger --stderr --priority user.debug --tag psyopsOS-postboot "Installing psyopsOS venv..."
+logger --stderr --priority user.debug --tag psyopsOS-init.sh "Installing psyopsOS venv..."
 python3 -m venv --upgrade-deps /var/psyopsOS/venv
-logger --stderr --priority user.debug --tag psyopsOS-postboot "Installing progfiguration..."
-/var/psyopsOS/venv/bin/python -m pip install /var/psyopsOS/"$progfig"
+logger --stderr --priority user.debug --tag psyopsOS-init.sh "Installing progfiguration..."
+/var/psyopsOS/venv/bin/python -m pip install --force-reinstall /var/psyopsOS/"$progfig"
 ln -sf /var/psyopsOS/venv/bin/psyopsOS-progfiguration /usr/local/sbin/psyopsOS-progfiguration
-logger --stderr --priority user.debug --tag psyopsOS-postboot "Running progfiguration..."
+logger --stderr --priority user.debug --tag psyopsOS-init.sh "Running progfiguration..."
 psyopsOS-progfiguration apply "$PSYOPSOS_NODENAME"
-logger --stderr --priority user.debug --tag psyopsOS-postboot "Finished running progfiguration"
+logger --stderr --priority user.debug --tag psyopsOS-init.sh "Finished running progfiguration"
 
 date +'%Y%m%d-%H%M%S %z' > /etc/psyopsOS/status/001-async-init.finished
