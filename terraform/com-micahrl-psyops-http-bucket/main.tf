91025cca25e3f92c5cc6ec0d07196a1ecb5ac260
Micah R Ledbetter <me@micahrl.com>
2022-09-27 18:39:33 -0500

    Only cache psyops.micahrl.com stuff for a minute
    Prevents issue where APKINDEX is updated but clients still get the old cached version

--- terraform/com-micahrl-psyops-http-bucket/main.tf
+++ terraform/com-micahrl-psyops-http-bucket/main.tf
@@ -175,8 +175,8 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "${local.psyops_http_bucket_domain}"
     min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
+    default_ttl            = 60
+    max_ttl                = 60
 
     forwarded_values {
       query_string = false
