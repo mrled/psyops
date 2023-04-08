ee3e2417cf086a85843fb32a2670d9a7ba9d551e
Micah R Ledbetter <me@micahrl.com>
2023-04-07 22:44:52 -0500

    Create ~/.local/bin in container

--- docker/home/.entrypoint.sh
+++ docker/home/.entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 cat "/usr/share/zoneinfo/${PSYOPS_TIMEZONE}" > /etc/localtime
+
+# $HOME/.local/bin is where pip installs --user packages, among other things
+mkdir -p "$HOME/.local/bin"
+
+# Sets the $PATH and a bunch of other stuff
 $HOME/.dhd/opt/bin/dhd-shdetect
+
 exec ssh-agent /usr/bin/tmux -u
