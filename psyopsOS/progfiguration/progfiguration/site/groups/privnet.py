7ee5f324359caaa0e0c486b5ac688fdeda489fbf
Micah R Ledbetter <me@micahrl.com>
2023-04-18 22:07:47 -0500

    Add privnet group

---
+++ psyopsOS/progfiguration/progfiguration/site/groups/privnet.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from progfiguration.inventory.invhelpers import Bunch
+from progfiguration.inventory.roles import RoleResultReference
+from progfiguration.localhost.disks import (
+    FilesystemSpec,
+    LvmLvSpec,
+    PartitionSpec,
+    WholeDiskSpec,
+)
+
+group = Bunch(
+    roles=Bunch(
+        blockdevparty={
+            "wholedisks": [
+                # Currently unused
+                WholeDiskSpec("/dev/nvme0n1", None, None, False),
+            ],
+            "partitions": [
+                PartitionSpec("/dev/sda", "psyopsosdata", "0%", "100%", volgroup="psyopsos_datadiskvg", encrypt=True),
+            ],
+            "volumes": [
+                LvmLvSpec("datadisklv", "psyopsos_datadiskvg", r"100%FREE", FilesystemSpec("ext4", "psyopsos_data")),
+            ],
+        },
+        acmeupdater_base={
+            "role_dir": Path("/psyopsos-data/roles/acmeupdater"),
+            "acme_email": "psyops@micahrl.com",
+        },
+        acmeupdater_synology={
+            "capthook_hooksdir": RoleResultReference("capthook", "hooksdir"),
+        },
+    ),
+)
