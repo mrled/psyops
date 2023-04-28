7d7ee228131a84ab9a5e81f39168f42a91b5eacc
Micah R Ledbetter <me@micahrl.com>
2023-04-27 23:44:53 -0500

    Handle nonexistent user crontab

--- psyopsOS/progfiguration/progfiguration/site/sitelib/__init__.py
+++ psyopsOS/progfiguration/progfiguration/site/sitelib/__init__.py
@@ -2,13 +2,22 @@
 
 import subprocess
 
+from progfiguration.localhost import LocalhostLinux
+
 
 def get_crontab(user: str) -> list[str]:
     """Get a user's system crontab as an array of lines"""
-    crontab = subprocess.run(
-        ["crontab", "-l", "-u", user], check=True, text=True, capture_output=True
-    ).stdout.splitlines()
-    return crontab
+    proc = subprocess.run(["crontab", "-l", "-u", user], text=True, capture_output=True)
+    if proc.returncode == 0:
+        return proc.stdout.splitlines()
+
+    # busybox crontab returns 1 from "crontab -lu" if the user has no crontab, wtf
+    # TODO: This shouldn't require class instantiation
+    localhost = LocalhostLinux()
+    if localhost.users.user_exists(user):
+        return []
+
+    raise ValueError(f"User {user} does not exist")
 
 
 def set_crontab(user: str, crontab: list[str]):
