d9a8da082ade8f20d784d40fcd217e39e1809abd
Micah R Ledbetter <me@micahrl.com>
2023-05-01 22:17:03 -0500

    Add siteglobals
    This isn't used yet

---
+++ psyopsOS/progfiguration/progfiguration/site/sitelib/siteglobals.py
@@ -0,0 +1,5 @@
+"""Constants and global variables for the site package."""
+
+psyops_ssh_pubkey = (
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ/zN5QLrTpjL1Qb8oaSniRQSwWpe5ovenQZOLyeHn7m conspirator@PSYOPS"
+)
