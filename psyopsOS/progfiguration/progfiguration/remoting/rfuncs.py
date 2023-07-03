bf03526c2d709d2e68411fd696a6d6eae4aa0c47
Micah R Ledbetter <me@micahrl.com>
2023-07-02 21:21:36 -0500

    Lobotomize progfiguration
    Move site to root/progfiguration/site, remove generic code
    
    This splits progfiguration into generic and site-specific components

--- psyopsOS/progfiguration/progfiguration/remoting/rfuncs.py
+++
@@ -1,9 +0,0 @@
-"""Functions that mitogen can run remotely"""
-
-
-import subprocess
-
-
-def sh(command: str):
-    result = subprocess.run(command, capture_output=True, shell=True)
-    return {"stdout": result.stdout, "stderr": result.stderr}
