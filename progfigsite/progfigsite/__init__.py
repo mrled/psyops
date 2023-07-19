f9b72aa12ca477dff571423351f4c786ff693d42
Micah R Ledbetter <me@micahrl.com>
2023-07-19 00:24:19 -0500

    Fix inventory section, remove package_inventory_file, add package name/desc

--- progfigsite/progfigsite/__init__.py
+++ progfigsite/progfigsite/__init__.py
@@ -4,7 +4,8 @@ This includes everything that is not generic to how progfiguration works,
 but is specific to my hosts/roles/groups/functions/etc.
 """
 
-import importlib.resources
+site_name = "psyops progfigsite"
 
-
-package_inventory_file = importlib.resources.files("progfigsite").joinpath("inventory.conf")
+# A slogan from a USACAPOC psychological operations challenge coin I found online,
+# whose provenance I did not investigate
+site_description = "BECAUSE PHYSICAL WOUNDS HEAL"
