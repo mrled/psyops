ff9baa644282a2fe6f91f73a9a2a7b2f65fe1923
Micah R Ledbetter <me@micahrl.com>
2023-12-08 18:20:19 -0600

    Change tkconfig imlementation transparently
    Don't use dataclass

--- telekinesis/src/telekinesis/config.py
+++ telekinesis/src/telekinesis/config.py
@@ -5,10 +5,8 @@ This is necessarily full of implementation details and local paths.
 Secrets should be stored in 1Password, and accessed with the getsecret function.
 """
 
-from dataclasses import dataclass
 from pathlib import Path
 import subprocess
-from typing import Callable
 
 
 def getsecret(item: str, field: str) -> str:
@@ -72,43 +70,37 @@ class TelekinesisConfig:
             self.sqfilename = "psyopsOSsq-psysquash-x86_64.iso"
             """The filename that our mkimage creates"""
 
-    @dataclass
     class TelekinesisBuildcontainerNode:
         """Configuration for the buildcontainer node"""
 
-        alpine_version: str
-        """The version of Alpine to use"""
-        dockertag_prefix: str
-        """The Docker image tag prefix (will be suffixed with the Alpine version)"""
-        get_signing_key: Callable[[], str]
-        """A function to retrieve the signing key from a password store"""
-        apk_key_filename: str
-        """This is the name to set for the APK signing key in the Alpine builder container.
-        It doesn't rely on any file with this name existing on the host."""
-        apkreponame: str
-        """The APK repo name
-        When building a package with abuild, it finds this name by looking in the grandparent directory of the APKBUILD file (../../).
-        See also progfiguration_blacksite's buildapk command, which forces this with a tempdir.
-        This is just called "repo=" in the abuild sh script, and is not configurable."""
-        uri: str
-        """The URI that is accessible on the"""
-        isotag: str
-        """Alpine ISO image tag for the boot image we build with mkimage.sh"""
-        sqtag: str
-        """Alpine squashfs image tag for the boot image we build with mkimage.sh"""
-        architecture: str
-        """The architecture for the ISO image"""
-        mkimage_iso_profile: str
-        """The Alpine mkimage.sh ISO profile for psyopOS,
-        which requires a profile_PROFILENAME file in the aports scripts overlay directory"""
-        mkimage_squashfs_profile: str
-        """The Alpine mkimage.sh ISO profile for psyopOS,
-        which requires a profile_PROFILENAME file in the aports scripts overlay directory"""
-
-        @property
-        def dockertag(self):
+        def __init__(self, alpine_version: str):
+            self.alpine_version = alpine_version
+            """The version of Alpine to use"""
+            self.dockertag_prefix = "psyopsos-builder-"
+            """The Docker image tag prefix (will be suffixed with the Alpine version)"""
+            self.get_signing_key = (lambda: getsecret("op://Personal/psyopsOS_abuild_ssh_key", "notesPlain"),)
+            """A function to retrieve the signing key from a password store"""
+            self.apk_key_filename = "psyops@micahrl.com-62ca1973.rsa"
+            """This is the name to set for the APK signing key in the Alpine builder container.
+            It doesn't rely on any file with this name existing on the host."""
+            self.apkreponame = "psyopsOS"
+            """The APK repo name
+            When building a package with abuild, it finds this name by looking in the grandparent directory of the APKBUILD file (../../).
+            See also progfiguration_blacksite's buildapk command, which forces this with a tempdir.
+            This is just called "repo=" in the abuild sh script, and is not configurable."""
+            self.uri = f"https://psyops.micahrl.com/"
+            """The URI that is accessible on the public Internet for the APK repository"""
+            self.isotag = "psyboot"
+            """Alpine ISO image tag for the boot image we build with mkimage.sh"""
+            self.sqtag = "psysquash"
+            """Alpine squashfs image tag for the boot image we build with mkimage.sh"""
+            self.architecture = "x86_64"
+            """The architecture for the ISO image"""
+            self.mkimage_iso_profile = "psyopsOScd"
+            """The Alpine mkimage.sh ISO profile for psyopOS,
+            which requires a profile_PROFILENAME file in the aports scripts overlay directory"""
+            self.dockertag = f"{self.dockertag_prefix}{self.alpine_version}"
             """The Docker image tag of the build container"""
-            return f"{self.dockertag_prefix}{self.alpine_version}"
 
     class TelekinesisConfigArtifactsNode:
         """Configuration for the artifacts node"""
@@ -154,19 +146,7 @@ class TelekinesisConfig:
     def __init__(self):
         self.repopaths = self.PsyopsRepoPaths(root=self.psyopsroot)
         self.deaddrop = self.TelekinesisConfigDeaddropNode(self.repopaths.artifacts)
-        self.buildcontainer = self.TelekinesisBuildcontainerNode(
-            alpine_version=self.alpine_version,
-            dockertag_prefix="psyopsos-builder-",
-            get_signing_key=lambda: getsecret("op://Personal/psyopsOS_abuild_ssh_key", "notesPlain"),
-            apk_key_filename="psyops@micahrl.com-62ca1973.rsa",
-            apkreponame="psyopsOS",
-            uri=f"https://psyops.micahrl.com/",
-            isotag="psyboot",
-            sqtag="psysquash",
-            architecture="x86_64",
-            mkimage_iso_profile="psyopsOScd",
-            mkimage_squashfs_profile="psyopsOSsq",
-        )
+        self.buildcontainer = self.TelekinesisBuildcontainerNode(self.alpine_version)
         self.artifacts = self.TelekinesisConfigArtifactsNode(self.repopaths.artifacts)
 
 
