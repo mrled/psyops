13f8612cc90aa371d80cac8368b595849b3a2a50
Micah R Ledbetter <me@micahrl.com>
2024-05-01 22:23:50 -0500

    Fix config directory in shhh

--- ansible/roles/seedbox/files/shhh-docker/entrypoint.sh
+++ ansible/roles/seedbox/files/shhh-docker/entrypoint.sh
@@ -7,8 +7,8 @@ W="$(echo Juvfcnee | tr 'A-Za-z' 'N-ZA-Mn-za-m')"
 w="$(echo juvfcnee | tr 'A-Za-z' 'N-ZA-Mn-za-m')"
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 groupadd -g $PGID "$w"
-useradd -u $PUID -g "$w" -d /opt/Juvfcnee -s /bin/sh -M "$w"
+useradd -u $PUID -g "$w" -d /config -s /bin/sh -M "$w"
 chown -R "$w":"$w" /opt/Juvfcnee
 ls -alF /opt/Juvfcnee
 # echo exec su - "$w" -c "$*"
-exec su - "$w" -c "/opt/Juvfcnee/$W -nobrowser -data /config"]
+exec su - "$w" -c "/opt/Juvfcnee/$W"
