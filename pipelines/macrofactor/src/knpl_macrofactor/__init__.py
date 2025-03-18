16e0ab2f7020e7887e1c5d7622bd7f60310d372e
Micah R Ledbetter <me@micahrl.com>
2025-03-18 18:51:21 -0500

    Fix versioning

---
+++ pipelines/macrofactor/src/knpl_macrofactor/__init__.py
@@ -0,0 +1,6 @@
+"""knpl_macrofactor package"""
+
+import os
+
+
+__version__ = os.environ.get("KNPL_MACROFACTOR_BUILD_VERSION", "0.1.0")
--- pipelines/macrofactor/src/knpl_macrofactor/version.py
+++
@@ -1,3 +0,0 @@
-import os
-
-__version__ = os.environ.get("KNPL_MACROFACTOR_BUILD_VERSION", "0.1.0")
