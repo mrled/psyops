38fe966eb1c27f5e5153bc082dae1fc3a47d2ad5
Micah R Ledbetter <me@micahrl.com>
2025-04-08 19:40:35 -0500

    Add dieselgrove

---
+++ progfiguration_blacksite/src/progfiguration_blacksite/nodes/dieselgrove.py
@@ -0,0 +1,15 @@
+from progfiguration.inventory.nodes import InventoryNode
+
+node = InventoryNode(
+    address="dieselgrove.home.micahrl.com",
+    user="root",
+    ssh_host_fingerprint="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGr3u+0NCBagU73urz6BZhohOYLBWUWTb0hBwzoDKadY mrled@Naragua",
+    sitedata=dict(
+        notes="",
+        flavortext="",
+        psy0mac="f4:4d:30:64:e0:2e",
+        serial="00000000",
+        age_pubkey="age1sgru2huy7ue4am3gks59zf8tmqc7yz6gjyv3sxmlsyd4wallg96st7fnef",
+    ),
+    roles=dict(),
+)
