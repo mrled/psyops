7e54c6ec275493dd42616f6b8005a7bee18ef5ab
Micah R Ledbetter <me@micahrl.com>
2025-04-13 14:57:36 -0500

    Add rusthaven and wirefield

---
+++ progfiguration_blacksite/src/progfiguration_blacksite/nodes/rusthaven.py
@@ -0,0 +1,15 @@
+from progfiguration.inventory.nodes import InventoryNode
+
+node = InventoryNode(
+    address="rusthaven.home.micahrl.com",
+    user="root",
+    ssh_host_fingerprint="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcoX6xh9IbulJYJY52M6y2ytczrG1cx5NdZfqWO5Owy mrled@Naragua",
+    sitedata=dict(
+        notes="",
+        flavortext="",
+        psy0mac="dc:a6:32:ab:ca:fc",
+        serial="942c266a",
+        age_pubkey="age1a99mnqdunk3rdu088860qww6jevkaftswmnd48xhc7h846ntgahscm8umt",
+    ),
+    roles=dict(),
+)
---
+++ progfiguration_blacksite/src/progfiguration_blacksite/nodes/wirefield.py
@@ -0,0 +1,15 @@
+from progfiguration.inventory.nodes import InventoryNode
+
+node = InventoryNode(
+    address="wirefield.home.micahrl.com",
+    user="root",
+    ssh_host_fingerprint="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRVNVsQGRUVMrWKdMig4c4rea4KNrpQS7SrOB+Sj1CC mrled@Naragua",
+    sitedata=dict(
+        notes="",
+        flavortext="",
+        psy0mac="d8:3a:dd:72:d3:10",
+        serial="7c38af83",
+        age_pubkey="age19tr5ce6tvlca7f8gf5hpnvk7fygpfnv6t8pwad6q6q8sf2l9futsxsc0hv",
+    ),
+    roles=dict(),
+)
