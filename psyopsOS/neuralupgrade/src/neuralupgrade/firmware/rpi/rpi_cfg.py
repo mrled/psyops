a350add8748abd76b9c1869adb650c55c789316c
Micah R Ledbetter <me@micahrl.com>
2025-04-14 22:23:05 -0500

    Fix U-Boot script file
    * Implement new method for finding the right A/B partitions
    * Fix many boot.cmd bugs
    * Load the dtb

--- psyopsOS/neuralupgrade/src/neuralupgrade/firmware/rpi/rpi_cfg.py
+++ psyopsOS/neuralupgrade/src/neuralupgrade/firmware/rpi/rpi_cfg.py
@@ -11,7 +11,14 @@ from neuralupgrade.firmware.timestamp import firwmare_update_timestamp
 
 
 # A string.Template that sets variables in the boot.cmd file.
-_boot_cmd_header_template = """\
+_boot_cmd_header_fmt = """\
+# {last_updated}
+# IMPORTANT: LEAVE THE LINE ABOVE THIS COMMENT ALONE.
+# This script doesn't know what partition contains it,
+# but it can ask U-Boot to find all partitions,
+# load boot.cmd if it exists,
+# and compare the contents of the first few bytes.
+
 # psyopsOS boot.cmd
 # Variables populated by neuralupgrade
 
@@ -20,31 +27,52 @@ _boot_cmd_header_template = """\
 ####
 
 # The date this was last updated
+# IMPORTANT: DO NOT CHANGE THE NAME OF THIS VARIABLE.
+# The length of the variable **name** must be accounted for in the memory comparison below.
 setenv last_updated {last_updated}
 
 # The partition labels for the A and B filesystems.
-setenv psyopsOS_A_label $A_label
-setenv psyopsOS_B_label $B_label
+setenv psyopsOS_A_label {A_label}
+setenv psyopsOS_B_label {B_label}
 
 # The default boot label (must be one of A or B)
-setenv default_boot_label $default_boot_label
+setenv default_boot_label {default_boot_label}
 
 # Starting addresses for kernel and initramfs.
 # These are JUST used during boot and do not affect OS runtime;
 # Linux will move itself around in memory as needed,
 # and initrd is mounted to a tmpfs instance at boot,
 # so the original memory buffer from U-Boot is freed.
-setenv kernel_addr_r $kernel_addr_r
-setenv ramdisk_addr_r $ramdisk_addr_r
+setenv kernel_addr_r {kernel_addr_r}
+setenv ramdisk_addr_r {ramdisk_addr_r}
+setenv dtb_addr_r {dtb_addr_r}
 
 # Kernel commandline parameters to each A/B side
-setenv kernel_params "$kernel_params"
+setenv kernel_params "{kernel_params}"
 
+"""
+
+_boot_cmd_body_simple = """\
+echo "Welcome to psyopsOS! Simple boot command loaded. Dropping to U-Boot shell..."
+echo "----------------------------------------------------------------------------"
+echo ""
+exit
 """
 
 # A string, with no variables, that uses the variables set in the header.
 _boot_cmd_body = """\
+echo "Welcome to psyopsOS. U-Boot configuration last updated: ${last_updated}"
+echo ""
+
+# Uncomment to drop into a shell early
+# echo "Dropping into U-Boot shell..."
+# exit
+
+# Initialize USB
+usb start
+
 # Clear previous bootmenu
+# TODO: is this really necessary?
 for i in 0 1 2 3 4 5; do
     setenv bootmenu_${i}
 done
@@ -54,59 +82,85 @@ done
 setenv probe_devtypes "mmc usb"
 # setenv probe_devtypes "mmc usb sata scsi"
 
-setenv psyopsOS_A_devtype ""
-setenv psyopsOS_A_devnum ""
-setenv psyopsOS_A_partnum ""
-setenv psyopsOS_B_devtype ""
-setenv psyopsOS_B_devnum ""
-setenv psyopsOS_B_partnum ""
-for devtype in ${devtypes}; do
+setenv bootdev_type ""
+setenv bootdev_num ""
+for devtype in ${probe_devtypes}; do
     for devnum in 0 1 2; do
+        echo "Probing ${devtype} ${devnum} for boot.cmd..."
         if ${devtype} dev ${devnum}; then
-            for partnum in 1 2 3 4 5 6 7 8; do
-                if part uuid ${devtype} ${devnum}:${partnum} uuid; then
-                    blkid uuid ${uuid} label
-                    if test "${label}" = "${psyopsOS_A_label}"; then
-                        # echo "Found ${psyopsOS_A_label} on ${devtype} ${devnum}:${partnum}"
-                        setenv psyopsOS_A_devtype ${devtype}
-                        setenv psyopsOS_A_devnum ${devnum}
-                        setenv psyopsOS_A_partnum ${partnum}
-                    elif test "${label}" = "${psyopsOS_B_label}"; then
-                        # echo "Found ${psyopsOS_B_label} on ${devtype} ${devnum}:${partnum}"
-                        setenv psyopsOS_B_devtype ${devtype}
-                        setenv psyopsOS_B_devnum ${devnum}
-                        setenv psyopsOS_B_partnum ${partnum}
-                    fi
+            # Assume that the boot partition will only ever be the first one on a device
+            if load ${devtype} ${devnum}:1 ${kernel_addr_r} /boot.cmd; then
+                echo "Found boot.cmd on ${devtype} ${devnum}:1"
+                # We know we found **SOME** boot.cmd, but we don't know if it's the one that's currently running.
+
+                # Because the load worked, ${kernel_addr_r} has a value of something like '# 20250415-003744...'
+                # Skip two characters to skip the '# ':
+                setexpr header_timestamp_r ${kernel_addr_r} + 0x2
+
+                # Now build a string in RAM that contains **this** boot.cmd's last_updated timestamp.
+                env export -t ${ramdisk_addr_r} last_updated
+                # Now ${ramdisk_addr_r} has a value of something like 'last_updated=20250415-003744'
+                # Skip 13 characters to skip the 'last_updated=':
+                setexpr this_timestamp_r ${ramdisk_addr_r} + 0xd
+
+                # Compare the two timestamps, which are 15 bytes each
+                if cmp.b ${header_timestamp_r} ${this_timestamp_r} 0xf; then
+                    echo "Found our own boot.cmd file on ${devtype} ${devnum}:0"
+                    setenv bootdev_type ${devtype}
+                    setenv bootdev_num ${devnum}
+                else
+                    echo "Found a DIFFERENT boot.cmd file on ${devtype} ${devnum}:0, not using it"
                 fi
-            done
+            else
+                echo "No boot.cmd found on ${devtype} ${devnum}:0"
+            fi
+        else
+            echo "No ${devtype} ${devnum} device found"
         fi
     done
 done
 
-# Exit with an error if it can't find anything to boot
-if test "${psyopsOS_A_devtype}" == "" && "${psyopsOS_B_devtype}" == ""; then
-    echo "ERROR: No psyopsOS partitions found!"
+if test "${bootdev_type}" = ""; then
+    echo "ERROR: No bootable device found!"
     # Drop into the U-Boot shell
     exit
 fi
 
-echo "Welcome to psyopsOS. U-Boot configuration last updated: {last_updated}"
-echo "----------------------------------------"
+setenv psyopsOS_A_partnum ""
+setenv psyopsOS_B_partnum ""
+for partnum in 2 3 4 5 6 7 8; do
+    if load ${bootdev_type} ${bootdev_num}:${partnum} ${kernel_addr_r} ${psyopsOS_A_label}; then
+        echo "Found ${A_label} on ${bootdev_type} ${bootdev_num}:${partnum}"
+        setenv psyopsOS_A_partnum ${partnum}
+    else
+        if load ${bootdev_type} ${bootdev_num}:${partnum} ${kernel_addr_r} ${psyopsOS_B_label}; then
+            echo "Found ${B_label} on ${bootdev_type} ${bootdev_num}:${partnum}"
+            setenv psyopsOS_B_partnum ${partnum}
+        fi
+    fi
+done
+
+# Exit with an error if it can't find anything to boot
+if test "${psyopsOS_A_partnum}" = "" && test "${psyopsOS_B_partnum}" = ""; then
+    echo "ERROR: No psyopsOS partitions found!"
+    # Drop into the U-Boot shell
+    exit
+fi
 
 # Fallback error message if default_boot_label is not found
 setenv default_bootcmd "echo "ERROR: Default boot label ${default_boot_label} not found! Exiting to U-Boot shell..."; exit;"
 
 # Boot commands for each label
 setenv menu_index 0
-if test "${psyopsOS_A_devtype}" != ""; then
+if test "${psyopsOS_A_partnum}" != ""; then
     setenv bootmenu_${menu_index} "${psyopsOS_A_label}=run boot_psyopsOS_A"
     if test "${default_boot_label}" = "${psyopsOS_A_label}"; then
         setenv default_bootcmd "run boot_psyopsOS_A"
     fi
     setexpr menu_index ${menu_index} + 1
 fi
-if test "${psyopsOS_B_devtype}" != ""; then
-    setenv bootmenu_${menu_index} "${psyopsOS_B_label}=run "
+if test "${psyopsOS_B_partnum}" != ""; then
+    setenv bootmenu_${menu_index} "${psyopsOS_B_label}=run boot_psyopsOS_B"
     if test "${default_boot_label}" = "${psyopsOS_B_label}"; then
         setenv default_bootcmd "run boot_psyopsOS_B"
     fi
@@ -125,25 +179,32 @@ setexpr menu_index ${menu_index} + 1
 setenv boot_psyopsOS_A "
   echo Booting psyopsOS_A...;
   setenv bootargs \"root=LABEL=${psyopsOS_A_label} ${kernel_params}\" &&
-  ext4load ${psyopsOS_A_devtype} ${psyopsOS_A_devnum}:${psyopsOS_A_partnum} ${kernel_addr_r} /kernel &&
-  ext4load ${psyopsOS_A_devtype} ${psyopsOS_A_devnum}:${psyopsOS_A_partnum} ${ramdisk_addr_r} /initrd &&
-  booti ${kernel_addr_r} ${ramdisk_addr_r} -
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_A_partnum} ${kernel_addr_r} /kernel &&
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_A_partnum} ${ramdisk_addr_r} /initramfs &&
+  setenv initrdsize ${filesize} &&
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_A_partnum} ${dtb_addr_r} /boot/dtbs-lts/broadcom/bcm2711-rpi-4-b.dtb &&
+  booti ${kernel_addr_r} ${ramdisk_addr_r}:${initrdsize} ${dtb_addr_r}
 "
 
 # Boot logic for psyopsOS_B
 setenv boot_psyopsOS_B "
   echo Booting psyopsOS_B...;
   setenv bootargs \"root=LABEL=${psyopsOS_B_label} ${kernel_params}\" &&
-  ext4load ${psyopsOS_B_devtype} ${psyopsOS_B_devnum}:${psyopsOS_B_partnum} ${kernel_addr_r} /kernel &&
-  ext4load ${psyopsOS_B_devtype} ${psyopsOS_B_devnum}:${psyopsOS_B_partnum} ${ramdisk_addr_r} /initrd &&
-  booti ${kernel_addr_r} ${ramdisk_addr_r} -
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_B_partnum} ${kernel_addr_r} /kernel &&
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_B_partnum} ${ramdisk_addr_r} /initramfs &&
+  setenv initrdsize ${filesize} &&
+  ext4load ${bootdev_type} ${bootdev_num}:${psyopsOS_B_partnum} ${dtb_addr_r} /boot/dtbs-lts/broadcom/bcm2711-rpi-4-b.dtb &&
+  booti ${kernel_addr_r} ${ramdisk_addr_r}:${initrdsize} ${dtb_addr_r}
 "
 
 # Timeout and start menu
 setenv bootmenu_delay 5
 echo "Will boot into ${default_boot_label} in ${bootmenu_delay} seconds..."
-bootmenu
-run default_bootcmd
+# TODO: MUST REBUILD U-BOOT TO ENABLE CONFIG_CMD_BOOTMENU
+# bootmenu
+# run default_bootcmd
+
+run boot_psyopsOS_A
 
 """
 
@@ -155,13 +216,15 @@ arm_64bit=1
 arm_boost=1
 
 # Use as little memory as possible for the GPU (requires raspberrypi-bootloader-cutdown package)
-gpu_mem=16
+# In theory this could be as low as 16 but 64 is more conservative
+gpu_mem=64
 
 # Disable bluetooth, which uses the serial UART (?)
 dtoverlay=disable-bt
 # Enable the UART in the GPIO pins
 enable_uart=1
 # Enable debug logging to the UART during boot, show early boot messages over serial
+# For when start4.elf is running, before U-Boot (or a kernel) is loaded
 uart_2ndstage=1
 
 # Boot into U-Boot
@@ -178,6 +241,7 @@ def rpi_boot_cmd(
     kernel_params_base: str = "earlyprintk=dbgp console=tty0 console=serial0,115200 loglevel=7",
     kernel_addr_r: str = "0x80200000",
     ramdisk_addr_r: str = "0x82200000",
+    dtb_addr_r: str = "0x8a200000",
 ) -> str:
     """Return a U-Boot boot.cmd file for psyopsOS.
 
@@ -199,24 +263,33 @@ def rpi_boot_cmd(
     kernel_addr_r:          The address of the kernel in RAM.
                             The default value of 0x80200000 is common,
                             as it gives enough room before it for U-Boot stuff.
+                            It is almost almost 2GiB after 0x0 start address:
+                            0x82200000 / 0x100000 = 2082.0
     ramdisk_addr_r:         The address of the initramfs in RAM.
                             The default value of 0x82200000 is 32MB after the kernel start address,
                             which is large enough for a particularly big kernel.
-                            As of 2025, the x86_64 kernels I'm building are 10MB.
+                            As of 2025, the Raspberry Pi kernels I'm using from Alpine are about 10MB.
+                            (0x82200000 - 0x80200000) / 0x100000 = 32.0
+    dtb_addr_r:             The address of the device tree blob in RAM.
+                            As of 2025, the Raspberry Pi ramdisks I'm building are about 55MB.
+                            0x8a200000 is 128MB from the ramdisk start address:
+                            (0x8a200000 - 0x82200000) / 0x100000 = 128.0
     """
     if updated is None:
         updated = datetime.datetime.now()
     sections = [
-        _boot_cmd_header_template.format(
+        _boot_cmd_header_fmt.format(
             A_label=filesystems.a.label,
             B_label=filesystems.b.label,
             last_updated=firwmare_update_timestamp(updated),
-            default_label=default_boot_label,
+            default_boot_label=default_boot_label,
             kernel_addr_r=kernel_addr_r,
-            default_boot_label=ramdisk_addr_r,
+            ramdisk_addr_r=ramdisk_addr_r,
+            dtb_addr_r=dtb_addr_r,
             kernel_params=f"{kernel_params_base} {kernel_params_append}".strip(),
         ),
         _boot_cmd_body,
+        # _boot_cmd_body_simple,
     ]
     return "\n\n".join(sections)
 
@@ -245,9 +318,6 @@ def write_rpi_cfgs_carefully(
     mountpoint: str,
     default_boot_label: str,
     updated: Optional[datetime.datetime] = None,
-    # The following are not meant to be changed normally
-    max_old_files: int = 10,
-    max_old_days: int = 30,
 ):
     """Write the Raspbery Pi config files carefully
 
