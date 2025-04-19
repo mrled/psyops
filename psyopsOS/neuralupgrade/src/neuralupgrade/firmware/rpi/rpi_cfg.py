fe96b8060945d7fd1c6e9e5decda6de2617a023e
Micah R Ledbetter <me@micahrl.com>
2025-04-19 08:29:39 -0500

    Improve U-Boot script
    - Skip checking for things we already found, which boots faster and with less noise
    - Don't probe for USB devices unless we need to, which is faster when booting from MMC
    - Allow dropping into U-Boot shell with ctrl-c
    - Add a big comment block about why dropping into U-Boot shell is so annoying

--- psyopsOS/neuralupgrade/src/neuralupgrade/firmware/rpi/rpi_cfg.py
+++ psyopsOS/neuralupgrade/src/neuralupgrade/firmware/rpi/rpi_cfg.py
@@ -65,57 +65,58 @@ _boot_cmd_body = """\
 echo "Welcome to psyopsOS. U-Boot configuration last updated: ${last_updated}"
 echo ""
 
-# Uncomment to drop into a shell early
-# echo "Dropping into U-Boot shell..."
-# exit
-
-# Initialize USB
-echo "psyopsOS: Initializing USB..."
-usb start
-echo "psyopsOS: USB initialized"
-
 # Probe for psyopsOS A and B partitions by their partition label.
 # Currently only look on mmc and usb devices because my only U-Boot platform is the Raspberry Pi.
-setenv probe_devtypes "mmc usb"
 # setenv probe_devtypes "mmc usb sata scsi"
+setenv probe_devtypes "mmc usb"
 
-echo "psyopsOS: Probing for boot.cmd on ${probe_devtypes} devices..."
+echo "Probing for psyopsOS boot.cmd on ${probe_devtypes} devices..."
 setenv bootdev_type ""
 setenv bootdev_num ""
 for devtype in ${probe_devtypes}; do
-    for devnum in 0 1 2; do
-        echo "Probing ${devtype} ${devnum} for boot.cmd..."
-        if ${devtype} dev ${devnum}; then
-            # Assume that the boot partition will only ever be the first one on a device
-            if load ${devtype} ${devnum}:1 ${kernel_addr_r} /boot.cmd; then
-                echo "Found boot.cmd on ${devtype} ${devnum}:1"
-                # We know we found **SOME** boot.cmd, but we don't know if it's the one that's currently running.
-
-                # Because the load worked, ${kernel_addr_r} has a value of something like '# 20250415-003744...'
-                # Skip two characters to skip the '# ':
-                setexpr header_timestamp_r ${kernel_addr_r} + 0x2
-
-                # Now build a string in RAM that contains **this** boot.cmd's last_updated timestamp.
-                env export -t ${ramdisk_addr_r} last_updated
-                # Now ${ramdisk_addr_r} has a value of something like 'last_updated=20250415-003744'
-                # Skip 13 characters to skip the 'last_updated=':
-                setexpr this_timestamp_r ${ramdisk_addr_r} + 0xd
-
-                # Compare the two timestamps, which are 15 bytes each
-                if cmp.b ${header_timestamp_r} ${this_timestamp_r} 0xf; then
-                    echo "Found our own boot.cmd file on ${devtype} ${devnum}:0"
-                    setenv bootdev_type ${devtype}
-                    setenv bootdev_num ${devnum}
-                else
-                    echo "Found a DIFFERENT boot.cmd file on ${devtype} ${devnum}:0, not using it"
-                fi
-            else
-                echo "No boot.cmd found on ${devtype} ${devnum}:0"
-            fi
-        else
-            echo "No ${devtype} ${devnum} device found"
+    if test "${bootdev_type}" = ""; then
+        if test ${devtype} = "usb"; then
+            # This takes a few seconds; only bother if we didn't find anything on mmc
+            echo "Initializing USB..."
+            usb start
+            echo "USB initialized"
         fi
-    done
+        for devnum in 0 1 2; do
+            if test "${bootdev_num}" = ""; then
+                echo "Probing ${devtype} ${devnum} for boot.cmd..."
+                if ${devtype} dev ${devnum}; then
+                    # Assume that the boot partition will only ever be the first one on a device
+                    if load ${devtype} ${devnum}:1 ${kernel_addr_r} /boot.cmd; then
+                        echo "Found boot.cmd on ${devtype} ${devnum}:1"
+                        # We know we found **SOME** boot.cmd, but we don't know if it's the one that's currently running.
+
+                        # Because the load worked, ${kernel_addr_r} has a value of something like '# 20250415-003744...'
+                        # Skip two characters to skip the '# ':
+                        setexpr header_timestamp_r ${kernel_addr_r} + 0x2
+
+                        # Now build a string in RAM that contains **this** boot.cmd's last_updated timestamp.
+                        env export -t ${ramdisk_addr_r} last_updated
+                        # Now ${ramdisk_addr_r} has a value of something like 'last_updated=20250415-003744'
+                        # Skip 13 characters to skip the 'last_updated=':
+                        setexpr this_timestamp_r ${ramdisk_addr_r} + 0xd
+
+                        # Compare the two timestamps, which are 15 bytes each
+                        if cmp.b ${header_timestamp_r} ${this_timestamp_r} 0xf; then
+                            echo "Found our own boot.cmd file on ${devtype} ${devnum}:0"
+                            setenv bootdev_type ${devtype}
+                            setenv bootdev_num ${devnum}
+                        else
+                            echo "Found a DIFFERENT boot.cmd file on ${devtype} ${devnum}:0, not using it"
+                        fi
+                    else
+                        echo "No boot.cmd found on ${devtype} ${devnum}:0"
+                    fi
+                else
+                    echo "No ${devtype} ${devnum} device found"
+                fi
+            fi
+        done
+    fi
 done
 
 if test "${bootdev_type}" = ""; then
@@ -127,14 +128,21 @@ echo "psyopsOS: Found bootable device: ${bootdev_type} ${bootdev_num}"
 echo "psyopsOS: Probing for psyopsOS A and B partitions..."
 setenv psyopsOS_A_partnum ""
 setenv psyopsOS_B_partnum ""
+setenv psyopsOS_A_msg "NOT FOUND"
+setenv psyopsOS_B_msg "NOT FOUND"
 for partnum in 2 3 4 5 6 7 8; do
-    if load ${bootdev_type} ${bootdev_num}:${partnum} ${kernel_addr_r} ${psyopsOS_A_label}; then
-        echo "Found ${A_label} on ${bootdev_type} ${bootdev_num}:${partnum}"
-        setenv psyopsOS_A_partnum ${partnum}
-    else
+    if test "${psyopsOS_A_partnum}" = ""; then
+        if load ${bootdev_type} ${bootdev_num}:${partnum} ${kernel_addr_r} ${psyopsOS_A_label}; then
+            echo "Found ${A_label} on ${bootdev_type} ${bootdev_num}:${partnum}"
+            setenv psyopsOS_A_partnum ${partnum}
+            setenv psyopsOS_A_msg "on ${bootdev_type} ${bootdev_num}:${partnum}"
+        fi
+    fi
+    if test "${psyopsOS_B_partnum}" = "" && test "${psyopsOS_A_partnum}" != "${partnum}"; then
         if load ${bootdev_type} ${bootdev_num}:${partnum} ${kernel_addr_r} ${psyopsOS_B_label}; then
             echo "Found ${B_label} on ${bootdev_type} ${bootdev_num}:${partnum}"
             setenv psyopsOS_B_partnum ${partnum}
+            setenv psyopsOS_B_msg "on ${bootdev_type} ${bootdev_num}:${partnum}"
         fi
     fi
 done
@@ -187,19 +195,55 @@ setenv bootb "
   echo "Booting kernel..." &&
   booti ${kernel_addr_r} ${ramdisk_addr_r}:\${initrdsize} ${dtb_addr_r}
 "
+
 # Make due with this message in lieu of bootmenu support,
 # which Alpine Linux U-Boot doesn't include.
+#
+# A note about exiting to the U-Boot shell:
+# "exit" stops execution of this script,
+# and immediately tries to boot via some fallback method that I can't seem to disable
+# that involves BOOTP and TFTP addresses like this from a Raspberry Pi 4 B:
+#   pxelinux.cfg/01-dc-a6-32-ab-ca-fc     This is the MAC address of the device
+#   pxelinux.cfg/C0A8011A                 This is the DHCP IP address of the device as an integer
+#   pxelinux.cfg/C0A8011                  Removing one digit at a time from the IP address
+#   pxelinux.cfg/C0A801                   ... etc down to just "C"
+#   pxelinux.cfg/default-arm-bcm283x-rpi
+#   pxelinux.cfg/default-arm-bcm283x
+#   pxelinux.cfg/default-arm
+#   pxelinux.cfg/default
+#   pxelinux.cfg/C0A8011A.img
+# That means that ctrl-c here will go through all those,
+# and you'll have to ctrl-c through each one to get to the U-Boot shell.
+# I can't seem to disable this behavior. I've tried:
+#   setenv preboot
+#   setenv autoload no
+#   setenv netretry 0
+#   setenv bootcmd ''
+#   setenv boot_targets "mmc usb"
+# I've also tried
+# - setenv bootcmd "exit", and other values
+# - using 'sh' instead of 'exit', which returns to the parent scope immediately when run noninteractively
+#
 echo "psyopsOS: Ready to boot"
 echo ""
 echo "Press ctrl-c to cancel and drop into U-Boot shell"
+echo "(U-Boot will then try to netboot a bunch of targets; ctrl-c repeatedly to cancel that too)"
 echo "Once in the shell:"
-echo "  run boota:      Boot psyopsOS-A"
-echo "  run bootb:      Boot psyopsOS-B"
+echo "  run boota:      Boot psyopsOS-A (${psyopsOS_A_msg})"
+echo "  run bootb:      Boot psyopsOS-B (${psyopsOS_B_msg})"
 echo "  bdinfo:         Show board information"
 echo "  reset:          Reboot the board"
 echo ""
 echo "Will boot into ${default_boot_label} in 5 seconds..."
+
+# "setenv bootdelay 5" doesn't work here
+# Not sure if that's because Alpine U-Boot is built with CONFIG_BOOTDELAY=2 and its only read once?
 sleep 5
+if test $? = 1; then
+    # ctrl-c in sleep returns 1.
+    echo "Dropping into U-Boot shell..."
+    exit
+fi
 
 # Boot commands for each label
 if test "${psyopsOS_A_partnum}" != ""; then
@@ -212,7 +256,7 @@ if test "${psyopsOS_B_partnum}" != ""; then
         run bootb
     fi
 fi
-echo "ERROR: Default boot label ${default_boot_label} not found! Dropping into U-Boot shell..."
+echo "ERROR: Default boot label ${default_boot_label} not found!"
 exit
 
 """
