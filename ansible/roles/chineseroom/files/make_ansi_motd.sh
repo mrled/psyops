e71f0f50aa9d2d7a17499cfd63de07dd24fa4d06
Micah R Ledbetter <me@micahrl.com>
2025-07-02 21:06:57 -0500

    Update the motd

--- ansible/roles/chineseroom/files/make_ansi_motd.sh
+++ ansible/roles/chineseroom/files/make_ansi_motd.sh
@@ -1,8 +1,8 @@
 make_ansi_motd() {
-    printf '\033[90m“Oh, right,” \033[3mRorschach\033[23m said suddenly.'
-    printf '“We get it \033[3mnow\033[23m. You don’t think there’s anyone here, do you? You’ve got some high-priced consultant telling you there’s nothing to worry about.\n'
-    printf '…\n'
-    printf '“Your mistake, \033[3mTheseus\033[23m.”\n'
+    printf '\033[90m'
+    printf '  “You don’t think there’s anyone here, do you?”\n'
+    printf '  — \033[3mRorschach\033[23m, in \033[3mBlindsight\033[23m by Peter Watts\n'
+    printf '\e[0m'
 }
 
 make_ansi_motd
