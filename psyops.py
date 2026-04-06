d89e1bb6eb45e6e280af60112510ba01a6d0361d
Micah R Ledbetter <me@micahrl.com>
2026-04-05 22:34:54 -0500

    Remove a bunch of functionality from psyops.py

--- psyops.py
+++ psyops.py
@@ -3,10 +3,8 @@
 """PSYOPS Docker wrapper to make life a bit easier"""
 
 import argparse
-import configparser
 import logging
 import os
-import shutil
 import subprocess
 import sys
 import textwrap
@@ -62,26 +60,7 @@ def dockerrun(
     hostname: str = "PSYOPS",
 ):
     """Run the Docker container"""
-
-    env = os.environ.copy()
-
-    # For fuck's sake
-    env["DOCKER_SCAN_SUGGEST"] = "false"
-
-    # On Windows, we need to set the MSYS_NO_PATHCONV flag to 1, or else volume
-    # mounting fails with weird errors
-    # https://lmonkiewicz.com/programming/get-noticed-2017/docker-problems-on-windows/
-    if sys.platform == "win32":
-        env["MSYS_NO_PATHCONV"] = "1"
-
     volperms = "rw"
-    # if sys.platform == "darwin":
-    #     # See also:
-    #     # - https://docs.docker.com/docker-for-mac/osxfs-caching/
-    #     # - https://www.docker.com/blog/user-guided-caching-in-docker-for-mac/
-    #     # This is only for legacy osxfs
-    #     volperms = "cached"
-
     volume = f"{SCRIPTDIR}:{psyopsvol}:{volperms}"
 
     runcli = [
@@ -104,7 +83,7 @@ def dockerrun(
     if containerargs:
         runcli += containerargs.split(" ")
     logger.info(f"Running an image with: {' '.join(runcli)}")
-    retcode = subprocess.call(runcli, env=env)
+    retcode = subprocess.call(runcli)
 
     suppressed_result_codes = [
         130,  # probably SIGINT aka ctrl-c from the last program
@@ -151,127 +130,6 @@ def dockerbuild(imagename: str, imagetag: str, additional_build_args: str = ""):
     subprocess.check_call(buildcli, env=env)
 
 
-def clean():
-    """Clean nonpersistent data
-
-    Almost everything is handled when exiting the docker container,
-    but some data persists in ~/.local so that eg pipenv sticks around between invocations.
-    """
-    home_local = os.path.join(SCRIPTDIR, ".home.psyops.local")
-    for c in os.listdir(home_local):
-        if c == ".keep":
-            continue
-        c_path = os.path.join(home_local, c)
-        if os.path.isdir(c_path):
-            shutil.rmtree(c_path)
-        else:
-            os.unlink(c_path)
-
-
-class GitRepoMetadata:
-    """Metadata of a checked-out Git repository"""
-
-    _submodules: list["GitRepoMetadata"] | None = None
-
-    def __init__(self, checkoutdir: str, dotgitdir: str = ""):
-        self.checkoutdir: str = checkoutdir
-        if dotgitdir:
-            self.dotgitdir = dotgitdir
-        else:
-            self.dotgitdir = os.path.join(checkoutdir, ".git")
-
-    @property
-    def submodules(self):
-        """Enumerate Git submodules
-
-        Given the checkout dir of parent Git repository, enumerate all
-        submodules
-        """
-
-        if self._submodules is None:
-            modconfig = configparser.ConfigParser()
-            # This will return an empty config object if the file doesn't exist
-            modconfig.read(os.path.join(self.checkoutdir, ".gitmodules"))
-            self._submodules = []
-            for section in modconfig.sections():
-                copath = modconfig[section]["path"]
-                dotgit = os.path.join(self.checkoutdir, ".git", "modules", copath)
-                self._submodules += [GitRepoMetadata(copath, dotgit)]
-
-        return self._submodules
-
-    def fixcrlf(self):
-        """Fix autocrlf in a single Git repo
-
-        On Windows, Git defaults to enabling 'core.autocrlf', which
-        translates between Windows and Unix line endings, such that the
-        repository is checked out with Windows line endings, but committed
-        with Unix line endings.
-        However, for Unix scripts to actually run in a Unix, we need the
-        Unix line endings, so we need to disable this feature on Windows.
-
-        Note that this is DESTRUCTIVE if there are any uncommitted changes,
-        so we check for those first and fail if any are present.
-
-        checkoutdir:    the directory the repository is checked out to
-        dotgitdir:      the .git directory of the parent repo, or
-                        .git/modules/<module path> for submodules
-        """
-
-        logger.info(f"Fixing CRLF for module at {self.checkoutdir}")
-
-        status = subprocess.check_output(["git", "status"], cwd=self.checkoutdir)
-        if "nothing to commit, working tree clean" not in str(status):
-            msg = f"Uncommitted changes in {self.checkoutdir}"
-            logger.warning(msg)
-            raise Exception(msg)
-
-        # First, change the setting
-        subprocess.check_call(
-            ["git", "config", "core.autocrlf", "off"], cwd=self.checkoutdir
-        )
-
-        # Now remove the existing index and reset the repo, so that it re-
-        # checks out each file in the repository without autocrlf
-        os.remove(
-            os.path.realpath(
-                os.path.normpath(
-                    os.path.expanduser(os.path.join(self.dotgitdir, "index"))
-                )
-            )
-        )
-        subprocess.check_call(["git", "reset", "--hard", "HEAD"], cwd=self.checkoutdir)
-
-    def allfixcrlf(self):
-        """Fix autocrlf in a repo and all its submodules
-
-        Note that we fix submodules first, since the main repo was likely
-        handled at checkout time anyway, so it may not be necessary to clear
-        any uncommitted changes and try againn
-        """
-        for submodule in self.submodules:
-            submodule.fixcrlf()
-        self.fixcrlf()
-
-    def testcheckout(self, throw: bool = False):
-        """Test that all submodules are checked out
-
-        If care is not taken when the initial repo is checked out, or when the
-        location of a submodule has changed, it can result in an empty
-        submodule checkout directory that is not detected until we attempt to
-        use it. Check for that here first and fail early.
-        """
-        missing: list[str] = []
-        for submodule in self.submodules:
-            submodpath = os.path.join(SCRIPTDIR, submodule.checkoutdir)
-            if not os.path.exists(os.path.join(submodpath, ".git")):
-                logger.warning(f"Submodule at {submodule.checkoutdir} not checked out")
-                missing += [submodpath]
-        if missing and throw:
-            raise Exception(f"These submodules are not properly checked out: {missing}")
-        return missing == []
-
-
 class PsyopsArgumentCollection:
     """PSYOPS argument collection
 
@@ -281,20 +139,7 @@ class PsyopsArgumentCollection:
 
     def __init__(self, args: list[str]):
 
-        description = textwrap.dedent(
-            """
-            Wrap Docker for PSYOPS
-
-            Basically, Docker can be a little precious sometimes.
-            This script is intended to handle some known problems for intended use
-            cases in this repo:
-
-            - The image we build assumes that this repo will be mounted as a volume
-            - Volumes in Windows wont work unless $env:MSYS_NO_PATHCONV is set
-            - Windows doesn't have &&, so you can't do 'docker build && docker run'
-
-            (This script is not intended to wrap all of Docker's functionality)"""
-        )
+        description = "Wrap Docker for PSYOPS"
         epilog = textwrap.dedent(
             """
             Note about the passthrough arguments:
@@ -368,14 +213,6 @@ class PsyopsArgumentCollection:
             help="Mount point for the secrets tmpfs filesystem",
         )
 
-        self.utilopts = argparse.ArgumentParser(add_help=False)
-        self.utilsubparsers = self.utilopts.add_subparsers(dest="utilaction")
-        self.utilsubparsers.required = True
-        self.utilsubparsers.add_parser("enumsubmod", help="List all Git submodules")
-        self.utilsubparsers.add_parser(
-            "fixcrlf", help="Disasble core.autocrlf on parent repo and all submodules"
-        )
-
         self.subparsers = self.parser.add_subparsers(dest="action")
         self.subparsers.required = True
         self.subparsers.add_parser(
@@ -392,14 +229,6 @@ class PsyopsArgumentCollection:
             "buildrun",
             parents=[self.dockeropts, self.dockerbuildopts, self.dockerrunopts],
             help="Build the Docker image, then immediately run it",
-        )
-        self.subparsers.add_parser(
-            "util", parents=[self.utilopts], help="Utility functions"
-        )
-
-        # Most cached data is thrown away, but there is stuff in e.g. ~/.local/
-        self.subparsers.add_parser(
-            "clean", help="Clean any cached data from running the container."
         )
 
         self.parsed = self.parser.parse_args(args=args)
@@ -418,15 +247,11 @@ def main(args: list[str]):  # pylint: disable=W0613
 
     logger.info(parsed)
 
-    parentrepo = GitRepoMetadata(SCRIPTDIR)
-
     if parsed.action == "build":
-        parentrepo.testcheckout(throw=True)
         dockerbuild(
             parsed.imagename, parsed.imagetag, additional_build_args=parsed.buildargs
         )
     elif parsed.action == "run":
-        parentrepo.testcheckout(throw=True)
         dockerrun(
             parsed.imagename,
             parsed.imagetag,
@@ -436,7 +261,6 @@ def main(args: list[str]):  # pylint: disable=W0613
             containerargs=parsed.containerargs,
         )
     elif parsed.action == "buildrun":
-        parentrepo.testcheckout(throw=True)
         dockerbuild(
             parsed.imagename, parsed.imagetag, additional_build_args=parsed.buildargs
         )
@@ -448,18 +272,6 @@ def main(args: list[str]):  # pylint: disable=W0613
             runargs=parsed.runargs,
             containerargs=parsed.containerargs,
         )
-    elif parsed.action == "util":
-        if parsed.utilaction == "enumsubmod":
-            for submod in parentrepo.submodules:
-                print(submod.checkoutdir)
-        elif parsed.utilaction == "fixcrlf":
-            parentrepo.allfixcrlf()
-        else:
-            print(f"Unknown utilaction: '{parsed.utilaction}'")
-            arguments.utilopts.print_help()
-            return 1
-    elif parsed.action == "clean":
-        clean()
     else:
         print(f"Unknown action '{parsed.action}'")
         arguments.parser.print_usage()
