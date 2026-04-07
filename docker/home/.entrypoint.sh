1d334eaec8d3577050a85754757a2df7d3dc0afd
Micah R Ledbetter <me@micahrl.com>
2026-04-06 20:31:50 -0500

    Implement brilliant plan to use AppArmor to contain Claude

--- docker/home/.entrypoint.sh
+++ docker/home/.entrypoint.sh
@@ -8,6 +8,10 @@ mkdir -p "$HOME/.local/bin"
 # Sets the $PATH and a bunch of other stuff
 $HOME/.dhd/opt/bin/dhd-shdetect
 
+# Set up AppArmor. This does nothing unless we run a command with aa-exec.
+sudo /sbin/apparmor_parser -r /etc/apparmor.d/claude
+sudo /usr/sbin/aa-enforce /etc/apparmor.d/claude
+
 mode=${1:-interactive}
 echo "Mode: $mode"
 case "$mode" in
@@ -15,7 +19,8 @@ case "$mode" in
     exec ssh-agent /usr/bin/tmux -u
     ;;
   claude)
-    exec claude
+    # AppArmor profile 'psyops-claude' must be loaded on the Docker host first.
+    exec aa-exec -p claude -- claude
     ;;
   *)
     echo "Unknown mode: $mode" >&2
